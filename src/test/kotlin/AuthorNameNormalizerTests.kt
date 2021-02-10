1deb40c473392fa2cb36c89dbd0c3f065e9e5ddc
Jon Reid <Jonathan.M.Reid@aexp.com>
2021-02-10 15:33:37 -0800

    Switch to assertk for assertions

--- src/test/kotlin/AuthorNameNormalizerTests.kt
+++ src/test/kotlin/AuthorNameNormalizerTests.kt
@@ -1,6 +1,6 @@
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import kotlin.test.assertFailsWith
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
@@ -16,73 +16,49 @@ class AuthorNameNormalizerTests {
     @Ignore
     @Test
     fun `returns empty string when empty`() {
-        assertThat(
-            normalizer.normalize(""),
-            equalTo("")
-        )
+        assertThat(normalizer.normalize("")).isEqualTo("")
     }
 
     @Ignore
     @Test
     fun `returns single word name`() {
-        assertThat(
-            normalizer.normalize("Plato"),
-            equalTo("Plato")
-        )
+        assertThat(normalizer.normalize("Plato")).isEqualTo("Plato")
     }
 
     @Ignore
     @Test
     fun `swaps first and last names`() {
-        assertThat(
-            normalizer.normalize("Haruki Murakami"),
-            equalTo("Murakami, Haruki")
-        )
+        assertThat(normalizer.normalize("Haruki Murakami")).isEqualTo("Murakami, Haruki")
     }
 
     @Ignore
     @Test
     fun `trims leading and trailing whitespace`() {
-        assertThat(
-            normalizer.normalize("  Big Boi   "),
-            equalTo("Boi, Big")
-        )
+        assertThat(normalizer.normalize("  Big Boi   ")).isEqualTo("Boi, Big")
     }
 
     @Ignore
     @Test
     fun `initializes middle name`() {
-        assertThat(
-            normalizer.normalize("Henry David Thoreau"),
-            equalTo("Thoreau, Henry D.")
-        )
+        assertThat(normalizer.normalize("Henry David Thoreau")).isEqualTo("Thoreau, Henry D.")
     }
 
     @Ignore
     @Test
     fun `does not initialize one letter middle name`() {
-        assertThat(
-            normalizer.normalize("Harry S Truman"),
-            equalTo("Truman, Harry S")
-        )
+        assertThat(normalizer.normalize("Harry S Truman")).isEqualTo("Truman, Harry S")
     }
 
     @Ignore
     @Test
     fun `initializes each of multiple middle names`() {
-        assertThat(
-            normalizer.normalize("Julia Scarlett Elizabeth Louis-Dreyfus"),
-            equalTo("Louis-Dreyfus, Julia S. E.")
-        )
+        assertThat(normalizer.normalize("Julia Scarlett Elizabeth Louis-Dreyfus")).isEqualTo("Louis-Dreyfus, Julia S. E.")
     }
 
     @Ignore
     @Test
     fun `appends suffixes to end`() {
-        assertThat(
-            normalizer.normalize("Martin Luther King, Jr."),
-            equalTo("King, Martin L., Jr.")
-        )
+        assertThat(normalizer.normalize("Martin Luther King, Jr.")).isEqualTo("King, Martin L., Jr.")
     }
 
     @Ignore
